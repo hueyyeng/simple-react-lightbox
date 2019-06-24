e043a659610e9fd35e95041300ca3c7e84f6bd8a
Michele Cocuccio <michele@nucreative.co.uk>
2019-06-24 22:25:59 +0100

    Improves the SRLLightbox by using the useCallback function.

--- src/SRL/SRLLightbox/SRLLightboxGallery/index.js
+++ src/SRL/SRLLightbox/SRLLightboxGallery/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import PropTypes from "prop-types";
 import { SRLLightboxGalleryStage } from "./styles";
 import SRLLightboxSlideComponent from "./SRLLightboxSlide";
@@ -19,10 +19,85 @@ const SRLLightboxGallery = ({
   const [currentImage, setCurrentImage] = useState(selectedImage);
   const [imagesGallery, ,] = useState(images);
 
+  // Handle Current Image
+  const handleCurrentImage = useCallback(
+    id => {
+      const imageSelected = _find(imagesGallery, function(i) {
+        return i.id === id;
+      });
+      setCurrentImage({
+        source: imageSelected.src,
+        caption: imageSelected.alt,
+        id: imageSelected.id,
+        width: imageSelected.width,
+        height: imageSelected.height
+      });
+    },
+    [imagesGallery]
+  );
+
+  // Handle Next Image
+  const handleNextImage = useCallback(
+    id => {
+      /* We receive the ID of the current image and we want the image after that.
+      Let's find the current position of the current image in the array */
+      const currentPosition = _findIndex(imagesGallery, function(i) {
+        return i.id === id;
+      });
+      /* The next image will be the next item in the array but it could be "undefined". If it's undefined we know we have reached the end and we go back to he first image */
+      const nextImage = imagesGallery[currentPosition + 1] || imagesGallery[0];
+      setCurrentImage({
+        source: nextImage.src,
+        caption: nextImage.alt,
+        id: nextImage.id,
+        width: nextImage.width,
+        height: nextImage.height
+      });
+    },
+    [imagesGallery]
+  );
+
+  const handlePrevImage = useCallback(
+    id => {
+      /* We receive the ID of the current image and we want the image after that.
+        Let's find the current position of the current image in the array */
+      // const currentPosition = imagesGallery.findIndex(i => i.id === id);
+      const currentPosition = _findIndex(imagesGallery, function(i) {
+        return i.id === id;
+      });
+      /* The prev image will be the prev item in the array but it could be "undefined" as it goes negative. If it does we need to start from the last image. */
+      const prevImage =
+        imagesGallery[currentPosition - 1] ||
+        imagesGallery[imagesGallery.length - 1];
+      setCurrentImage({
+        source: prevImage.src,
+        caption: prevImage.alt,
+        id: prevImage.id,
+        width: prevImage.width,
+        height: prevImage.height
+      });
+    },
+    [imagesGallery]
+  );
+
+  const handleLightboxWithKeys = useCallback(
+    event => {
+      if (event.keyCode === 39) {
+        handleNextImage(currentImage.id);
+      } else if (event.keyCode === 37) {
+        handlePrevImage(currentImage.id);
+      } else if (event.keyCode === 27) {
+        handleCloseLightbox();
+      }
+    },
+    [currentImage.id, handleCloseLightbox, handleNextImage, handlePrevImage]
+  );
+
   useEffect(() => {
     // SET THE CURRENT IMAGE TO THE BE THE FIRST IMAGE
+
     // This is crucial in case the user uses the provided method to open the lightbox from a link or a button etc...
-    if (selectedImage.id === undefined) {
+    if (currentImage.id === undefined) {
       setCurrentImage({
         source: imagesGallery[0].src,
         caption: imagesGallery[0].alt,
@@ -42,71 +117,14 @@ const SRLLightboxGallery = ({
       document.body.classList.remove("SRLOpened");
       document.removeEventListener("keydown", handleLightboxWithKeys, false);
     };
-  }, [handleLightboxWithKeys, imagesGallery, isOpened, selectedImage.id]);
-
-  // Handle Current Image
-  function handleCurrentImage(id) {
-    const selectedImage = _find(imagesGallery, function(i) {
-      return i.id === id;
-    });
-    setCurrentImage({
-      source: selectedImage.src,
-      caption: selectedImage.alt,
-      id: selectedImage.id,
-      width: selectedImage.width,
-      height: selectedImage.height
-    });
-  }
-
-  // Handle Next Image
-  function handleNextImage(id) {
-    /* We receive the ID of the current image and we want the image after that.
-    Let's find the current position of the current image in the array */
-    const currentPosition = _findIndex(imagesGallery, function(i) {
-      return i.id === id;
-    });
-    /* The next image will be the next item in the array but it could be "undefined". If it's undefined we know we have reached the end and we go back to he first image */
-    const nextImage = imagesGallery[currentPosition + 1] || imagesGallery[0];
-    setCurrentImage({
-      source: nextImage.src,
-      caption: nextImage.alt,
-      id: nextImage.id,
-      width: nextImage.width,
-      height: nextImage.height
-    });
-  }
-
-  // Handle Next Image
-  function handlePrevImage(id) {
-    /* We receive the ID of the current image and we want the image after that.
-      Let's find the current position of the current image in the array */
-    // const currentPosition = imagesGallery.findIndex(i => i.id === id);
-    const currentPosition = _findIndex(imagesGallery, function(i) {
-      return i.id === id;
-    });
-    /* The prev image will be the prev item in the array but it could be "undefined" as it goes negative. If it does we need to start from the last image. */
-    const prevImage =
-      imagesGallery[currentPosition - 1] ||
-      imagesGallery[imagesGallery.length - 1];
-    setCurrentImage({
-      source: prevImage.src,
-      caption: prevImage.alt,
-      id: prevImage.id,
-      width: prevImage.width,
-      height: prevImage.height
-    });
-  }
-
-  // Handle Lightbox with keys
-  function handleLightboxWithKeys(event) {
-    if (event.keyCode === 39) {
-      handleNextImage(currentImage.id);
-    } else if (event.keyCode === 37) {
-      handlePrevImage(currentImage.id);
-    } else if (event.keyCode === 27) {
-      handleCloseLightbox();
-    }
-  }
+  }, [
+    handleLightboxWithKeys,
+    handleCurrentImage,
+    imagesGallery,
+    isOpened,
+    selectedImage,
+    currentImage.id
+  ]);
 
   const controls = {
     currentImageId: currentImage.id,
