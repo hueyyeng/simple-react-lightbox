417ddb6b98f474fe69d03a9094b45c7ed0875d89
Michele Cocuccio <michelecocuccio@gmail.com>
2020-02-08 17:24:30 +0000

    Amends the code to use an actual thumbnail element instead of the image as thumbnail.

--- src/SRL/SRLLightbox/SRLLightboxGallery/SRLLightboxSlide/SRLLightboxThubnailGallery/index.js
+++ src/SRL/SRLLightbox/SRLLightboxGallery/SRLLightboxSlide/SRLLightboxThubnailGallery/index.js
@@ -23,7 +23,7 @@ const SRLLightboxThubnailGalleryComponent = ({
               currentId === i.id ? 'SRLSelected' : ''
             }`}
             style={{
-              backgroundImage: `url(${i.source})`
+              backgroundImage: `url(${i.thumbnail})`
             }}
           />
         )
