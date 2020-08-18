d2cc01cd3b3f3f273ab7cf633cfd3704562f41a7
Michele <michelecocuccio@gmail.com>
2020-08-18 23:38:51 +0200

    Adds example with Gatsby images

---
+++ example-with-gatsby/gatsby-browser.js
@@ -0,0 +1,14 @@
+/**
+ * Implement Gatsby's Browser APIs in this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/browser-apis/
+ */
+
+import './src/styles/index.scss'
+import React from 'react'
+import SimpleReactLightbox from 'simple-react-lightbox'
+
+// eslint-disable-next-line react/prop-types
+export const wrapRootElement = ({ element }) => (
+  <SimpleReactLightbox>{element}</SimpleReactLightbox>
+)
